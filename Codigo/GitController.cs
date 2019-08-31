a0b07edbbefe25e5ea9553f16a0d287313486c80
jose alexander <jose_delacruz4@usmp.pe>
2019-08-31 10:59:13 -0500

    primeros cambios

---
+++ Codigo/GitController.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace mynamespace{
+    class MyClass{
+        public String name {get;set;}
+        public String age {get;set;}
+        public String email {get;set;}
+    }
+}
